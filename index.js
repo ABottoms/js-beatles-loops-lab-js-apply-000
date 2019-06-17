c6a2e10442cedafcac09d87d9dac8a9b105eb730
salubrious-chain-5511 <anisa.bottoms@gmail.com>
2019-06-17 17:57:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,7 @@
 
 
 // function 'johnLennonFacts'
-   int [4] <johnLennonFacts> + ('!!!'):
-   print ["He was the last Beatle to learn to drive",
-          "He was never a vegetarian",
-          "He was a choir boy and a boy scout",
-          "He hated the sound of his own voice"];
-  
+   int [4] <johnLennonFacts>:
+   print["He was the last Beatle to learn to drive!!!",     "He was never a vegetarian!!!",
+         "He was a choir boy and boy scout!!!", 
+         "He hated the sound of his own voice!!!"]
