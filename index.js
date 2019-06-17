c2d6d5000967defd66ae61afa1fd70e107b62c08
salubrious-chain-5511 <anisa.bottoms@gmail.com>
2019-06-17 17:29:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,8 @@
 
 
 // while <johnLennonFacts>: 
-   print []
+   print [
+          'He was the last Beatle to learn to drive'
+          'He was never a vegetarian'
+          'He was a choir boy and a boy scout'
+          'He hated the sound of his own voice'];
